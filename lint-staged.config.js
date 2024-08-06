d3a3a0b11bb6fc6d80167756afd30a88e2906f65
Abe Chen <abe1272001@gmail.com>
2024-08-06 18:59:00 +0800

    test: lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   // this will check Typescript files
-  '**/*.(ts|tsx)': () => 'pnpm tsc --noEmit',
+  '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
 
   // This will lint and format TypeScript and JavaScript files
   '**/*.(ts|tsx|js)': (filenames) => [
-    `pnpm eslint --fix ${filenames.join(' ')}`,
-    `pnpm prettier --write ${filenames.join(' ')}`,
+    `npx eslint --fix ${filenames.join(' ')}`,
+    `npx prettier --write ${filenames.join(' ')}`,
   ],
 
   // this will Format MarkDown and JSON
   '**/*.(md|json|mdx)': (filenames) =>
-    `pnpm prettier --write ${filenames.join(' ')}`,
+    `npx prettier --write ${filenames.join(' ')}`,
 }
