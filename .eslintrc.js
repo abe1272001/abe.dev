53a257ae879c903942976d4038a014726d4daffc
Abe Chen <abe1272001@gmail.com>
2024-08-06 19:25:29 +0800

    chore: update eslint and prettier setting to sync each other

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,6 +46,9 @@ module.exports = {
       },
     ],
     'react/jsx-props-no-spreading': 'off',
+    'import/order': 'off',
+    semi: ['error', 'never'], // 強制不使用分號
+    quotes: ['error', 'single'], // 強制使用單引號
     'import/extensions': [
       'error',
       'ignorePackages',
@@ -67,6 +70,12 @@ module.exports = {
     'no-nested-ternary': 'off',
     'import/prefer-default-export': 'off',
     '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'warn',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
     'tailwindcss/classnames-order': 'off', // Respect prettier-plugin-tailwindcss order
+    'comma-dangle': ['error', 'always-multiline'],
   },
 }
