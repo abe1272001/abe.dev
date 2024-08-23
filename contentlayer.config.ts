6626d48709890696b3d59dba6dd6e996be358172
Abe Chen <abe1272001@gmail.com>
2024-08-23 16:29:29 +0800

    feat: add contentlayer plugins

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -8,11 +8,18 @@ import {
 } from 'contentlayer2/source-files'
 import { slug } from 'github-slugger'
 import readingTime from 'reading-time'
+// Rehype packages
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeCodeTitles from 'rehype-code-titles'
-import rehypePrism from 'rehype-prism-plus'
+import rehypeKatex from 'rehype-katex'
+// import rehypeMermaid from 'rehype-mermaid'
+import rehypePresetMinify from 'rehype-preset-minify'
+import rehypePrismPlus from 'rehype-prism-plus'
 import rehypeSlug from 'rehype-slug'
+// Remark packages
 import remarkGfm from 'remark-gfm'
+import { remarkAlert } from 'remark-github-blockquote-alert'
+import remarkMath from 'remark-math'
 
 import siteMetadata from './data/siteMetadata'
 import { allCoreContent, sortPosts } from './utils/contentlayer.js'
@@ -115,11 +122,14 @@ export default makeSource({
   contentDirPath: 'data',
   documentTypes: [Post, Author],
   mdx: {
-    remarkPlugins: [remarkGfm],
+    remarkPlugins: [remarkGfm, remarkMath, remarkAlert],
     rehypePlugins: [
       rehypeSlug,
       rehypeCodeTitles,
-      [rehypePrism, { ignoreMissing: true }],
+      [rehypePrismPlus, { ignoreMissing: true }],
+      rehypeKatex,
+      // FIXME: Ask author
+      // rehypeMermaid,
       [
         rehypeAutolinkHeadings,
         {
@@ -128,6 +138,7 @@ export default makeSource({
           },
         },
       ],
+      rehypePresetMinify,
     ],
   },
   onSuccess: async (importData) => {
